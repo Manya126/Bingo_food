31913fb7dbb5da3760615cc89125cd9600bab32f
Manya <manyakumari222@gmail.com>
2025-09-14 22:31:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const app=express()
 const server=http.createServer(app)
 const io=new Server(server,{
      cors: {
-    origin: "https://vingo-d8ad.onrender.com" // production में specific domain डालना
+    origin: ["http://localhost:5173", "https://vingo-d8ad.onrender.com"], // production में specific domain डालना
     methods: ["GET", "POST"],
     credentials: true  
   }
